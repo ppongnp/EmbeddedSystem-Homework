6423a787ba30a5d68e2362783f92b46fe0fcab2f
ppongnp <ppgxnp@gmail.com>
2020-02-21 00:42:36 +0700

    decorate the UI

--- hw2/3.Timer_press/main.cpp
+++ hw2/3.Timer_press/main.cpp
@@ -3,15 +3,14 @@
 #include "stm32f413h_discovery_ts.h"
 #include "stm32f413h_discovery_lcd.h"
 
-
 TS_StateTypeDef  TS_State = {0};
 DigitalIn  User_Button(PA_0,PullNone);
 Timeout t;
-void reset();
-
 int score;
-int attr;
 bool start;
+
+void reset();
+void start_and_end_screen_setup();
 
 int main() {
     start = false;
@@ -20,39 +19,54 @@ int main() {
     if (BSP_TS_Init(BSP_LCD_GetXSize(), BSP_LCD_GetYSize()) == TS_ERROR) {
         printf("BSP_TS_Init error\n");
     }
-    BSP_LCD_Clear(LCD_COLOR_YELLOW);
-    BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
-    BSP_LCD_SetFont(&Font16);
-    BSP_LCD_DisplayStringAt(0, 120, (uint8_t *)"PRESS BUTTON TO START!", CENTER_MODE);
+    start_and_end_screen_setup();
+    BSP_LCD_DisplayStringAt(0, 120, (uint8_t *)"PRESS BUTTON TO START", CENTER_MODE);
 
     while(true) {  
       BSP_TS_GetState(&TS_State);
       if(User_Button.read() == 1 && start == false){ 
-          attr = 1;
           start = true;
           t.attach(&reset, 5.0);
-          BSP_LCD_Clear(LCD_COLOR_WHITE);
-          BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
-          BSP_LCD_DisplayStringAt(0, 120, (uint8_t *)"gogogo", CENTER_MODE);
+          BSP_LCD_Clear(LCD_COLOR_RED);
+          BSP_LCD_SetFont(&Font16);
+          BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
+          BSP_LCD_SetBackColor(LCD_COLOR_RED);  
+          BSP_LCD_DisplayStringAt(0, 120, (uint8_t *)"TOUCH HERE !!", CENTER_MODE);
       }
-
       if(TS_State.touchDetected && start == true){
           score += 1;
-          BSP_LCD_Clear(LCD_COLOR_WHITE);
-          BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
-          char s2[20]; 
-          sprintf(s2, "%d", score); 
-          BSP_LCD_DisplayStringAt(0, 120, (uint8_t *)s2, CENTER_MODE);
+          char temp[20]; 
+          sprintf(temp, "%d", score);
+          BSP_LCD_Clear(LCD_COLOR_RED);
+          BSP_LCD_SetFont(&Font24);
+          BSP_LCD_DisplayStringAt(0, 120, (uint8_t *)temp, CENTER_MODE);
           thread_sleep_for(100);
       }
   }
 }
+void start_and_end_screen_setup(){
+    BSP_LCD_Clear(LCD_COLOR_BLUE);
+    BSP_LCD_SetFont(&Font16);
+    BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
+    BSP_LCD_SetBackColor(LCD_COLOR_BLUE);    
+}
 
 void reset(){
     t.detach();
-    score = 0;
     start = false;
-    BSP_LCD_Clear(LCD_COLOR_WHITE);
-    BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
-    BSP_LCD_DisplayStringAt(0, 120, (uint8_t *)"END", CENTER_MODE);
+
+    char temp[20]; 
+    sprintf(temp, "%d", score);
+    char result[] = "YOUR SCORE ARE :";
+    strcat(result, temp);
+
+    start_and_end_screen_setup();
+    BSP_LCD_SetFont(&Font24);
+    BSP_LCD_DisplayStringAt(0, 60, (uint8_t *)"GAME OVER!", CENTER_MODE);
+    BSP_LCD_SetFont(&Font16);
+    BSP_LCD_DisplayStringAt(0, 160, (uint8_t *)result, CENTER_MODE);
+    BSP_LCD_SetFont(&Font12);
+    BSP_LCD_DisplayStringAt(0, 200, (uint8_t *)"PRESS BUTTON TO TRY AGAIN!", CENTER_MODE);
+
+    score = 0;
 }
